b208578a95109b1c3159892c6794d961dc688852
Jared Haralson <jharalson29@gmail.com>
2022-02-12 11:27:47 -0500

    Event and Photo routes now work. Inserted more Users.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const eventRoutes = require('./eventRoutes');
-// const photoRoutes = require('./photoRoutes');
+const photoRoutes = require('./photoRoutes');
 
 router.use('/users', userRoutes);
 router.use('/events', eventRoutes);
-// router.use('/photos', photoRoutes);
+router.use('/photos', photoRoutes);
 
 module.exports = router;
