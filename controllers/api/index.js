1343e4c6d3d4a4d541f5502a6ceca352e95ba124
Aubrey Crook <aubreycrook@hotmail.com>
2022-02-15 20:21:14 -0500

    created weather route and handlebars file which it will render

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const userRoutes = require('./userRoutes');
 const eventRoutes = require('./eventRoutes');
 const photoRoutes = require('./photoRoutes');
 const rsvpRoutes = require('./rsvpRoutes')
+const {weatherRouter} = require('./weatherroute')
 
 router.use('/users', userRoutes);
 router.use('/events', eventRoutes);
 router.use('/photos', photoRoutes);
 router.use('/rsvp', rsvpRoutes);
+router.use('/weather', weatherRouter);
 
 module.exports = router;
