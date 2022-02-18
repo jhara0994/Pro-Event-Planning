54d3a12ef58ad4b8e419c9b4e67bae2a1383918a
Shaquille Rivera <shaq.rivera@gmail.com>
2022-02-18 13:05:34 -0500

    Fixed create post feature

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,22 +1,40 @@
-// const newFormHandler = async (event) => {
-//     event.preventDefault();
+const eventSubmit = document.getElementById('eventSubmit');
+const userId = document.getElementById('userId');
+userId.style.display = 'none';
+const newFormHandler = async (event) => {
+    event.preventDefault();
   
-//     const title = document.querySelector('#project-name').value.trim();
-//     const description = document.querySelector('#project-funding').value.trim();
+    const title = document.getElementById('eventTitle').value.trim();
+    const description = document.getElementById('eventDescription').value.trim();
+    const date = document.getElementById('eventDate').value.trim();
+    const city = document.getElementById('eventCity').value.trim();
+    const state = document.getElementById('eventState').value.trim();
+    const fee = document.getElementById('eventFee').value.trim();
+    
+
+    if (title && description && date && city && state) {
+      const response = await fetch(`/api/events`, {
+        method: 'POST',
+        body: JSON.stringify({ 
+            title: title, 
+            event_description: description,
+            event_date: date,
+            location_city: city,
+            location_state: state,
+            fee: fee,
+            user_id: userId.textContent
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
   
-//     if (title && description) {
-//       const response = await fetch(`/api/eventRoutes`, {
-//         method: 'POST',
-//         body: JSON.stringify({ title, description }),
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       });
-  
-//       if (response.ok) {
-//         document.location.replace('/dashboard');
-//       } else {
-//         alert('Failed to create event');
-//       }
-//     }
-//   };
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create event');
+      }
+    }
+  };
+
+  eventSubmit.addEventListener('click', newFormHandler);
