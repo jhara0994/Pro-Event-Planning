8d20076ef1f5642189d861c42317f0bf94924403
Jared Haralson <jharalson29@gmail.com>
2022-02-18 18:03:52 -0500

    Initial commit to Heroku

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    'heroku_fe0dcfab6eaee08',
+    'bb12d5d5ed812c',
+    '6c8f2aae',
     {
-      host: 'localhost',
+      host: 'us-cdbr-east-05.cleardb.net',
       dialect: 'mysql',
       port: 3306
     }
