98a4b12f942f25bf0603a5a50f29ba953d6ab8d0
Jared Haralson <jharalson29@gmail.com>
2022-02-17 22:00:28 -0500

    Pushing changes to multiple files before pushing to main

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,9 +13,10 @@ router.get("/", async (req, res) => {
     });
 
     const events = eventData.map((Events) => Events.get({ plain: true }));
-    res.render("homepage", {
+    res.render("homepage", { data: {
       events,
-      loggedIn: req.session.loggedIn,
+      loggedIn: req.session.logged_in,
+    }
     });
   } catch (err) {
     console.log(err);
@@ -51,6 +52,26 @@ router.get("/event/:id", async (req, res) => {
     res.status(500);
   }
 });
+
+// GET user dashboard 
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password']},
+      include: {model: Events}
+    })
+
+    const user = userData.get({ plain: true })
+
+    res.render('dashboard', { data: {
+      user,
+      logged_in: true
+    }
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 // router.post('/registration/:event_id/:user_id', async (req, res) =>{
 //   try {
