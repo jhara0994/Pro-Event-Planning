0007261714068661241d083faeea4309669eb862
Shaquille Rivera <shaq.rivera@gmail.com>
2022-02-18 21:12:35 -0500

    merge conflict

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const eventRoutes = require('./eventRoutes');
 const photoRoutes = require('./photoRoutes');
-const {weatherRouter} = require('./weatherroute');
+const weatherRoutes = require('./weatherroute');
 const rsvpRoutes = require('./rsvpRoutes');
 const guestRoutes = require('./guestRoutes');
 
@@ -10,7 +10,7 @@ router.use('/users', userRoutes);
 router.use('/events', eventRoutes);
 router.use('/photos', photoRoutes);
 router.use('/rsvp', rsvpRoutes);
-router.use('/weather', weatherRouter);
+router.use('/weather', weatherRoutes);
 router.use('/guests', guestRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const cron = require('node-cron');
-
+const {sendEmail} = require('./utils/sendEmail.js');
+const { Events, User, Rsvp} = require('./models');
+const request = require('postman-request');
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
@@ -43,10 +45,32 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log("Now listening");
-  //Need to use cron.schedule to run the a weather query daily, and then send an email based on the results.
-//   cron.schedule('*/5 * * * * *', function() {
-//   console.log('running a task every 5 seconds');
-// });
+  cron.schedule('*/5 * * * * *', async function() {
+  console.log('running a task every 5 seconds');
+  ;
+  const eventData = await Events.findAll(
+    {include: Rsvp}
+  );
+  const events = eventData.map((event) => event.get({plain: true}));
+  
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
+    let eventDate = new Date(event.event_date);
+    let today = new Date(Date.now())
+    let oneDay = 1000 * 60 * 60 * 24;
+    let daysOut = Math.floor(((eventDate - today)/oneDay));
+    if (daysOut < 6){
+    request(`http://localhost:3307/api/weather/${event.location_city}/${daysOut}`, function (error, response, body) {
+        if (error){console.log('error:', error);}; // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body);
+      });
+    }
+    
+
+    
+  }
+});
 }
   );
 });
