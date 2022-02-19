df4c6d81f43648fcca478c68e21e6b45b5f41c3b
Shaquille Rivera <shaq.rivera@gmail.com>
2022-02-18 19:34:42 -0500

    Changed connection variables for production

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    'heroku_e9dd7ba12403b4f?reconnect=true',
+    'b9e3de41fc99fc',
+    '666069f3',
     {
-      host: 'localhost',
+      host: 'us-cdbr-east-05.cleardb.net',
       dialect: 'mysql',
       port: 3306
     }
