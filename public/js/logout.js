0f2ac88f01902dfd81faddf74a75ced93f44aeae
Shaquille Rivera <shaq.rivera@gmail.com>
2022-02-19 02:23:43 -0500

    Fixed logout button and configured to send email every 24 hours

--- public/js/logout.js
+++ public/js/logout.js
@@ -2,6 +2,7 @@ const logout = async () => {
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
+      mode: 'no-cors'
     });
   
     if (response.ok) {
--- server.js
+++ server.js
@@ -45,30 +45,86 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log("Now listening");
-  cron.schedule('*/5 * * * * *', async function() {
-  console.log('running a task every 5 seconds');
+  cron.schedule('* * */23 * * *', async function() {
+  console.log('running a task every 23 hours');
   ;
   const eventData = await Events.findAll(
     {include: Rsvp}
   );
+  let emails = [];
   const events = eventData.map((event) => event.get({plain: true}));
-  
-  for (let i = 0; i < events.length; i++) {
+  for (let i = 0; i < events.length; i++){
     const event = events[i];
+    let rsvps = events[i].rsvps;
+    emails.length = 0;
+    let userIds = rsvps.map((rsvp)=>{
+      return {user_id: rsvp.user_id}
+    });
+    
+    for (let i = 0; i < userIds.length; i++) {
+      const userId = userIds[i];
+      const userData = await User.findByPk(userId.user_id);
+      const user = userData.get({plain: true});
+      if(user.email){
+      emails.push(user.email);  
+      }
+      
+     
+    }
+    console.log(emails);
     let eventDate = new Date(event.event_date);
     let today = new Date(Date.now())
     let oneDay = 1000 * 60 * 60 * 24;
     let daysOut = Math.floor(((eventDate - today)/oneDay));
     if (daysOut < 6){
-    request(`http://localhost:3307/api/weather/${event.location_city}/${daysOut}`, function (error, response, body) {
+    request(`http://localhost:3307/api/weather/${event.location_city}/${daysOut}`, async function (error, response, body) {
         if (error){console.log('error:', error);}; // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body);
-      });
-    }
-    
+        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //console.log('body:', body);
+      if(body){
+        body = JSON.parse(body);
+       function kelvintocelsius(kelvin){
+          let celsius = kelvin - 273.15;
+          return celsius;
+      
+      }
+      function  celsiustofahrenheit(celsius){
+          let fahrenheit = celsius * 9/5 + 32;
+          return fahrenheit;
+      
+      }
+      function kelvintofahrenheit (kelvin){
+          return Math.floor(celsiustofahrenheit(kelvintocelsius(kelvin)));
+          
+      }
+      let high = kelvintofahrenheit(body.temp.max);
+      let low = kelvintofahrenheit(body.temp.min);
+      await sendEmail(emails, `Weather Update for ${event.title}`, '', `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Weather for ${event.title}</title>
+        </head>
+        <body>
+         <h1>Your Weather Update for ${event.title}</h1>
+        
+        <h2>${event.location_city}: ${event.event_date}</h2>
+        
+        <p>Expected Weather: ${body.weather[0].main}: ${body.weather[0].description}</p>
+        <p>High: ${high}°F </p>
+        <p>Low: ${low}°F </p>
+        
+        <h2>Enjoy your time at ${event.title}</h2>   
+        </body>
+        </html>` )
 
-    
+         
+      }
+      
+      });
+    } 
   }
 });
 }
