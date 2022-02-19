3a91cf319621a8103007b09a817246b6424c3007
Jared Haralson <jharalson29@gmail.com>
2022-02-19 10:21:35 -0500

    last final push to Heroku

--- config/connection.js
+++ config/connection.js
@@ -7,21 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-<<<<<<< HEAD
-<<<<<<< HEAD
     'heroku_fe0dcfab6eaee08',
     'bb12d5d5ed812c',
     '6c8f2aae',
-=======
-    'heroku_e9dd7ba12403b4f?reconnect=true',
-    'b9e3de41fc99fc',
-    '666069f3',
->>>>>>> df4c6d8 (Changed connection variables for production)
-=======
-    'heroku_6bb1a85ff64ed6a',
-    'bc4161daa20872',
-    '85d0c14',
->>>>>>> 8bd942f (Fixed variables)
     {
       host: 'us-cdbr-east-05.cleardb.net',
       dialect: 'mysql',
