0ce1030195ac1b6f27435efd6dca91b03beb6da9
Shaquille Rivera <shaq.rivera@gmail.com>
2022-02-19 02:56:56 -0500

    Added plaintext compiler to email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,7 +1,8 @@
 const nodemailer = require("nodemailer");
+const htmlToText = require('nodemailer-html-to-text').htmlToText;
 
 // async..await is not allowed in global scope, must use a wrapper
-async function sendEmail(recipients, subject = '', body = '', htmlBody = '') {
+async function sendEmail(recipients, subject = '', htmlBody = '') {
   let dotenv = require("dotenv").config();
 
   // create reusable transporter object using the default SMTP transport
@@ -15,11 +16,11 @@ async function sendEmail(recipients, subject = '', body = '', htmlBody = '') {
       pass: `${process.env.GMAIL_PASS}`, // generated ethereal password
     },
   });
+  transporter.use('compile', htmlToText());
   let mailOptions = {
     from: `Pro Event Planner 🌦" <${process.env.GMAIL_USER}>'`, // sender address
     to: `${recipients}`, // list of receivers
     subject: `${subject}`, // Subject line
-    text: `${body}`, // plain text body
     html: `${htmlBody || body}`, // html body
   };
   // send mail with defined transport object
